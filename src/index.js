7d7a675bd37160b1bf4d516c56e1f7e5b516aab8
Bar Sela <barsela03@gmail.com>
2022-03-13 15:57:07 +0200

    add validation

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
-const express= require('express');
-const port=process.env.PORT || 3000;
-const app= express();
+const express = require("express");
+const port = process.env.PORT || 3000;
+const app = express();
+require("dotenv").config();
+const { auth } = require("express-openid-connect");
+app.use(
+  auth({
+    authRequired: false,
+    auth0Logout: true,
+    issuerBaseURL: process.env.ISSUER_BASE_URL,
+    baseURL: process.env.BASE_URL,
+    clientID: process.env.CLIENT_ID,
+    secret: process.env.SECRET,
+  })
+);
 
-app.get('/', (req, res)=>{
-    res.send('<h>hi from server </h>');
+app.get("/", (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? "Logged in" : "Logged out");
 });
-app.listen(port,()=>{console.log('server is up and running');});
+app.listen(port, () => {
+  console.log("server is up and running");
+});
