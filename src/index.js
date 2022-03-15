65384941e2224fa5e822ff3114f36169f03b55f5
Bar Sela <barsela03@gmail.com>
2022-03-15 19:54:06 +0200

    update new branch

--- src/index.js
+++ src/index.js
@@ -1,26 +1,35 @@
-
 const express = require("express");
 const port = process.env.PORT || 3000;
 const app = express();
 
-//load and cache JavaScript modules. 
-const ejs = require('ejs');
+//load and cache JavaScript modules.
+const ejs = require("ejs");
 
 // Set EJS as templating engine
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
-app.use(express.urlencoded({
-  extended: true
-}));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
-app.get('/', (req, res)=>{
- 
+app.get("/", (req, res) => {
+  // The render method takes the name of the HTML
+  // page to be rendered as input
+  // This page should be in the views folder
+  // in the root directory.
+  res.render("pages/homePage");
+});
 
-  //res.render('pages/index');
-   
-  });
+app.get("/login", (req, res) => {
+  res.render("pages/login");
+});
 
-  app.listen(port, () => {
-    console.log('server is up and running');
-    //check
-  });
+app.get("/signUp", (req, res) => {
+  res.render("pages/signUp");
+});
+
+app.listen(port, () => {
+  console.log("server is up and running");
+});
